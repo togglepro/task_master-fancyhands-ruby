33b9b47b15ce2de1ad9d1e82e9b85e7823576ee8
Sean Devine <barelyknown@icloud.com>
2015-07-27 09:03:51 -0400

    Use symbol keys in data for call_window_start and _end to avoid oauth error.

--- lib/fancyhands/v1/outgoing.rb
+++ lib/fancyhands/v1/outgoing.rb
@@ -21,14 +21,13 @@ module FancyHands
       }
 
       if call_window_start and call_window_end
-        data['call_window_start'] = call_window_start.strftime("%Y-%m-%dT%H:%M:%SZ")
-        data['call_window_end'] = call_window_end.strftime("%Y-%m-%dT%H:%M:%SZ")
+        data[:call_window_start] = call_window_start.strftime("%Y-%m-%dT%H:%M:%SZ")
+        data[:call_window_end] = call_window_end.strftime("%Y-%m-%dT%H:%M:%SZ")
       end
 
       return @client.request.post("call/outgoing", data)
     end
-  
-  
+
     def get(key="", status="", cursor="")
       data = {
         :key => key,
