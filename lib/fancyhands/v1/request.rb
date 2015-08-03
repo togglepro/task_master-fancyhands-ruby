8a5a556f5c4997a884ddebdd3de8420b54d06fa1
Sean Devine <barelyknown@icloud.com>
2015-08-03 09:16:23 -0400

    Namespace the Request class within FancyHands module.

--- lib/fancyhands/v1/request.rb
+++ lib/fancyhands/v1/request.rb
@@ -1,42 +1,44 @@
 require "oauth"
-class Request
+module FancyHands
+  class Request
 
-  def initialize(key, secret, url)
-    @url = url
-    @consumer = OAuth::Consumer.new(key, secret)
-  end
-
-  def post(piece, data="")
-    response = @consumer.request(:post, @url + piece, nil, {}, data)
-    return JSON.parse(response.body)
-  end
-
-  def delete(piece, data="")
-    if data
-      uri = Addressable::URI.new
-      uri.query_values = data
-      data = uri.query
+    def initialize(key, secret, url)
+      @url = url
+      @consumer = OAuth::Consumer.new(key, secret)
     end
-    full = @url + piece + "?" + data
-    response = @consumer.request(:delete, full)
-    return JSON.parse(response.body)
-  end
 
-  def put(piece, data="")
-    # {'Content-Type' => 'application/x-www-form-urlencoded'}
-    response = @consumer.request(:put, @url + piece, nil, {}, data)
-    return JSON.parse(response.body)
-  end
-
-  def get(piece, data="")
-    if data
-      uri = Addressable::URI.new
-      uri.query_values = data
-      data = uri.query
+    def post(piece, data="")
+      response = @consumer.request(:post, @url + piece, nil, {}, data)
+      return JSON.parse(response.body)
     end
-    full = @url + piece + "?" + data
-    response = @consumer.request(:get, full)
-    return JSON.parse(response.body)
-  end
 
+    def delete(piece, data="")
+      if data
+        uri = Addressable::URI.new
+        uri.query_values = data
+        data = uri.query
+      end
+      full = @url + piece + "?" + data
+      response = @consumer.request(:delete, full)
+      return JSON.parse(response.body)
+    end
+
+    def put(piece, data="")
+      # {'Content-Type' => 'application/x-www-form-urlencoded'}
+      response = @consumer.request(:put, @url + piece, nil, {}, data)
+      return JSON.parse(response.body)
+    end
+
+    def get(piece, data="")
+      if data
+        uri = Addressable::URI.new
+        uri.query_values = data
+        data = uri.query
+      end
+      full = @url + piece + "?" + data
+      response = @consumer.request(:get, full)
+      return JSON.parse(response.body)
+    end
+
+  end
 end
